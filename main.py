1b3fd026f30299550437d1b53c0cfb2a53532851
Flokss <47820520+Flokss@users.noreply.github.com>
2019-02-25 15:02:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,149 @@
+"""
+Технически, плагины являются модулями и мало чем отличаются от базовых систем загружаемых в loader.py,
+но т.к. "модули" уже заняты - пусть будут плагины. Основные отличия плагинов:
+- Хранятся в директории src/plugins/, содержимое которой занесено в .gitignore терминала.
+- Загружаются и запускаются динамически после запуска всех базовых систем терминала. Первыми завершают свою работу.
+- Терминал никак не зависит от плагинов.
+
+Путь до плагина выглядит как mdmTerminal2/src/plugins/folder/main.py, где:
+folder - директория с плагином, ее имя не имеет значения.
+main.py - динамически загружаемый файл модуля. Терминал игнорирует остальные файлы и никак их не использует.
+
+Для успешной инициализации плагина, main.py должен содержать определенные свойства и точку входа (Main).
+"""
+import spidev
+import time
+import sys
+
+
+"""
+Обязательно. Не пустое имя плагина, тип - str.
+Максимальная длинна 30 символов, не должно содержать пробельных символов, запятых и быть строго в нижнем регистре.
+Имя плагина является его идентификатором и должно быть уникально.
+"""
+NAME = 'ws2812'
+
+
+"""
+Обязательно. Версия API под которую написан плагин, тип - int.
+Если оно меньше config.ConfigHandler.API то плагин не будет загружен, а в лог будет выдано сообщение.
+API терминала увеличивается когда публичные методы, их вызов или результат вызова (у cfg, log, owner) изменяется.
+API не увеличивается при добавлении новых методов.
+Призван защитить терминал от неправильной работы плагинов.
+Если API не используется или вам все равно, можно задать заведомо большое число (999999).
+"""
+API = 30
+
+
+SETTINGS = 'ws2812_config'
+spi = spidev.SpiDev()
+
+
+class Main:
+    """
+    Обязательно. Точка входа в плагин, должна быть callable.
+    Ожидается что это объект класса, экземпляр которого будет создан, но может быть и методом.
+    Должен принять 3 аргумента, вызов: Main(cfg=self.cfg, log=self._get_log(name), owner=self.own)
+    Может содержать служебные методы и свойства, все они опциональны. Методы должны быть строго callable:
+    Методы: start, reload, stop, join.
+    Свойства: disable.
+    """
+
+    def __init__(self, cfg, log, owner):
+        """
+        Конструктор плагина.
+        :param cfg: ссылка на экземпляр config.ConfigHandler.
+        :param log: ссылка на специальный логгер, вызов: log(msg, lvl=logger.Debug)
+        :param owner: ссылка на экземпляр loader.Loader
+        """
+        global spi_num
+        global m_intensity
+        global n_led
+        self.cfg = cfg
+        self.log = log
+        self.own = owner
+        self._settings = self._get_settings()
+        spi_num = self._settings['spi']
+        m_intensity = self._settings['intensity']
+        n_led = self._settings['num_led']
+        self._events = ('start_record', 'stop_record', 'start_talking', 'stop_talking')
+        self.disable = False
+
+    @staticmethod
+    def _init():
+        spi.open(spi_num, 0)
+
+
+    def _led_off(self):
+        #switch all nLED chips OFF.
+        self.write2812( spi, [[0,0,0]]*n_led)
+
+
+    def _talking(self, spi):
+        step_time = 0.1
+        i_step = 0
+        for ld in range(n_led):
+            d = [[0, 0, 0]] * n_led
+            d[i_step % n_led] = [m_intensity] * 3
+            self.write2812( spi, d)
+            i_step = (i_step + 1) % n_led
+            time.sleep(step_time)
+        self._led_off()
+
+    def _record(self, spi):
+        step_time = 0.01
+        for ld in range(m_intensity):
+            d = [[0, 0, ld]] * n_led
+            self.write2812( spi, d)
+            time.sleep(step_time)
+
+
+    def start(self):
+        self._init()
+        self._led_off()
+        self.own.subscribe(self._events, self._callback)
+
+
+
+    def stop(self):
+        self.own.unsubscribe(self._events, self._callback)
+        self._led_off()
+
+    def _callback(self, name, *_, **__):
+        if name == 'start_talking':
+            self._talking(spi)
+            self.log('start_talking')
+        elif name == 'stop_talking':
+            self._led_off()
+            self.log('stop_talking')
+        elif name == 'start_record':
+            self._record(spi)
+            self.log('start_record')
+        elif name == 'stop_record':
+            self._led_off()
+            self.log('stop_record')
+
+    def _get_settings(self) -> dict:
+        def_cfg = {'spi': 1,  'num_led': 8, 'intensity':30}
+        cfg = self.cfg.load_dict(SETTINGS)
+        if isinstance(cfg, dict):
+            is_ok = True
+            for key, val in def_cfg.items():
+                if key not in cfg or not isinstance(cfg[key], type(val)):
+                    is_ok = False
+                    break
+            if is_ok:
+                return cfg
+        self.cfg.save_dict(SETTINGS, def_cfg, True)
+        return def_cfg
+
+    def write2812(self, spi, data):
+        tx = [0x00]
+        for rgb in data:
+            for byte in rgb:
+                for ibit in range(3, -1, -1):
+                    tx.append(((byte >> (2 * ibit + 1)) & 1) * 0x60 +
+                              ((byte >> (2 * ibit + 0)) & 1) * 0x06 +
+                              0x88)
+        # print [hex(v) for v in tx]
+        spi.xfer(tx, int(4 / 1.05e-6))
